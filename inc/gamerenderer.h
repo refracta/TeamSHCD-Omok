69d525553d20c6ac1dfd438ec6124d0891b312c0
refracta <58779799+refracta@users.noreply.github.com>
2020-11-13 14:29:28 +0900

    fix RG_* enum to macro

--- inc/gamerenderer.h
+++ inc/gamerenderer.h
@@ -1,8 +1,14 @@
-typedef enum
-{
-	RG_BLACK = "¡Ü", RG_WHITE = "¡Û", 
-	RG_GRID_TYPE_1 = "¦£", RG_GRID_TYPE_2 = "¦¨", RG_GRID_TYPE_3 = "¦¤",
-	RG_GRID_TYPE_4 = "¦§", RG_GRID_TYPE_5 = "¦«", RG_GRID_TYPE_6 = "¦©",
-	RG_GRID_TYPE_7 = "¦¦", RG_GRID_TYPE_8 = "¦ª", RG_GRID_TYPE_9 = "¦¥",
-	RG_GRID_TYPE_H = "¦¡", RG_GRID_TYPE_V = "¦¢"
-} RenderGlyph;
+#define RG_BLACK "¡Ü"
+#define RG_WHITE "¡Û"
+
+#define RG_GRID_TYPE_1 "¦£"
+#define RG_GRID_TYPE_2 "¦¨"
+#define RG_GRID_TYPE_3 "¦¤"
+#define RG_GRID_TYPE_4 "¦§"
+#define RG_GRID_TYPE_5 "¦«"
+#define RG_GRID_TYPE_6 "¦©"
+#define RG_GRID_TYPE_7 "¦¦"
+#define RG_GRID_TYPE_8 "¦ª"
+#define RG_GRID_TYPE_9 "¦¥"
+#define RG_GRID_TYPE_H "¦¡"
+#define RG_GRID_TYPE_V = "¦¢"
