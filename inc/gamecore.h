6ade7bb472d7a85a69fb97e39dc4f0eb77f51be2
refracta <58779799+refracta@users.noreply.github.com>
2020-11-13 14:09:52 +0900

    add game enums

---
+++ inc/gamecore.h
@@ -0,0 +1,9 @@
+typedef enum
+{
+	BLACK_WIN, WHITE_WIN, DRAW, UNKNOWN
+} GameResult;
+
+typedef enum
+{
+	SG_BLACK = 'b', SG_WHITE = 'w', SG_EMPTY= '.'
+} StoneGlyph;
---
+++ inc/gamerenderer.h
@@ -0,0 +1,8 @@
+typedef enum
+{
+	RG_BLACK = "¡Ü", RG_WHITE = "¡Û", 
+	RG_GRID_TYPE_1 = "¦£", RG_GRID_TYPE_2 = "¦¨", RG_GRID_TYPE_3 = "¦¤",
+	RG_GRID_TYPE_4 = "¦§", RG_GRID_TYPE_5 = "¦«", RG_GRID_TYPE_6 = "¦©",
+	RG_GRID_TYPE_7 = "¦¦", RG_GRID_TYPE_8 = "¦ª", RG_GRID_TYPE_9 = "¦¥",
+	RG_GRID_TYPE_H = "¦¡", RG_GRID_TYPE_V = "¦¢"
+} RenderGlyph;
