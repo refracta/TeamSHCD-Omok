c45d1e161973ea412dcb2f89e326aac2dca1a360
nns503 <nns503@naver.com>
2020-11-21 18:29:48 +0900

    Update gamecore.c
    bool check_double_three
    bool check_double_four
    bool check_overlin
    bool check_confirm
    추가 하였습니다.

--- gamecore.c
+++ gamecore.c
@@ -3,6 +3,7 @@
   @brief 게임 코어
 */
 #include <stdlib.h>
+#include <stdbool>
 #include "inc/gamecore.h"
 
 /**
@@ -68,12 +69,10 @@ char** generate_grid(int width, int height)
 
 
 
-int RenjuRule(char** grid, int x, int y ) // 수정 예정
+bool check_double_three(char** grid, int x, int y)
 {
-	// 흑의 턴만 작동함 
-
+	// 흑의 턴
 		/*b=검은돌 w=흰돌 */
-		/*return cant -> 금수일 경우 리턴 */
 
 			// 가로 
 	int countblack = 1; // 두는 곳은 흑색이므로 1 추가
@@ -145,8 +144,6 @@ int RenjuRule(char** grid, int x, int y ) // 수정 예정
 	if (countempty - countwhite > 3 && opencheck = 1 && countblack == 3)
 		33check += 1;  // 가로 방향 33
 
-	///////////////////////////////////////////////////////////////////////
-
 		// 세로 
 	countblack = 1;
 	countempty = 0;
@@ -197,6 +194,11 @@ int RenjuRule(char** grid, int x, int y ) // 수정 예정
 
 		if (grid[y - i][x] == w)
 		{
+			countwhite++;
+			break;
+		}
+		else if (grid[y - i][x] != w)
+		{
 			if (grid[y - i][x] == b)
 			{
 				countblack++;
@@ -205,11 +207,6 @@ int RenjuRule(char** grid, int x, int y ) // 수정 예정
 			{
 				countempty++;
 			}
-		}
-		else if (grid[y - i][x] != w)
-		{
-			countwhite++;
-			break;
 		}
 		// 4번째 칸이 백돌일 경우 추가 진행
 		if (i == 3 && y - i - 1 < 0)
@@ -221,7 +218,6 @@ int RenjuRule(char** grid, int x, int y ) // 수정 예정
 	if (countempty - countwhite > 3 && opencheck = 1 && countblack == 3)
 		33check += 1;  // 세로 방향 33 
 
-	/////////////////////////////////////////////////////////////////////////
 
 		// 우측으로 떨어지는 대각선
 
@@ -262,7 +258,7 @@ int RenjuRule(char** grid, int x, int y ) // 수정 예정
 
 	// 한쪽이 열린 3인지 체킹
 	if (countempty > countwhite)
-		openncheck = 1;
+		opencheck = 1;
 
 	// 대각선 위쪽
 	for (int i = 1; i < 4; i++)
@@ -297,7 +293,6 @@ int RenjuRule(char** grid, int x, int y ) // 수정 예정
 	if (countempty - countwhite > 3 && opencheck = 1 && countblack == 3)
 		33check += 1;  // 우측으로 떨어지는 대각선 33 판정
 
-	/////////////////////////////////////////////////////////////
 
 		// 좌측으로 떨어지는 대각선
 	countblack = 1;
@@ -372,23 +367,25 @@ int RenjuRule(char** grid, int x, int y ) // 수정 예정
 		33check += 1;  // 좌측으로 떨어지는 대각선 33 판정
 
 
-// 3*3 판정에서 가로,세로,우측 대각선, 좌측 대각선에서 2개 이상이 판정 될 경우, 3*3으로 취급하여 금수가 됨.
+    // 3*3 판정에서 가로,세로,우측 대각선, 좌측 대각선에서 2개 이상이 판정 될 경우, 3*3으로 취급하여 금수가 됨.
 	if (33check >= 2)
 	{
-		return cant;  // 금수 처리
+		return false;  // 금수 처리
 	}
 
+	return true;
+}
 
 
-
-	// 4*4 판별
-
-			// 가로
+bool check_double_four(char** grid, int x, int y)
+{
+	int countblack = 1; // 두는 곳은 흑색이므로 1 추가
+	int countempty = 0;
+	int countwhite = 0;
 	int 44check = 0;
-	countblack = 1;
-	countempty = 0;
-	countwhite = 0;
+	int opencheck = 0;
 
+	//가로
 	for (int i = 1; i < 5; i++) {
 		if (x + i > 18)
 			break;
@@ -436,8 +433,6 @@ int RenjuRule(char** grid, int x, int y ) // 수정 예정
 	// 열린 4인 경우
 	if (countempty >= countwhite && countblack == 4)
 		44check += 1;  // 가로 방향 44 
-
-	///////////////////////////////////////////////////
 
 		// 세로
 	countblack = 1;
@@ -493,8 +488,6 @@ int RenjuRule(char** grid, int x, int y ) // 수정 예정
 	// 열린 4인 경우
 	if (countempty >= countwhite && countblack == 4)
 		44check += 1; // 세로 방향 44 
-
-	////////////////////////////////////////////////////
 
 		// 우측으로 떨어지는 대각선
 	countblack = 1;
@@ -554,8 +547,6 @@ int RenjuRule(char** grid, int x, int y ) // 수정 예정
 	// 열린 4인 경우
 	if (countempty >= countwhite && countblack == 4)
 		44check = 1;  // 우측으로 떨어지는 대각선 44 
-
-	/////////////////////////////////////////////////
 
 		// 좌측으로 떨어지는 대각선
 
@@ -620,8 +611,135 @@ int RenjuRule(char** grid, int x, int y ) // 수정 예정
 	// 4*4 판정에서 가로,세로,우측 대각선, 좌측 대각선에서 2개 이상이 판정 될 경우, 4*4으로 취급하여 금수가 됨.
 	else if (44check >= 2)
 	{
-		return cant;  // 금수 처리
+		return false;  // 금수 처리
 	}
 
-	// 예외 처리 들어가야됨
+
+	char copygrid[19][19] == {0};
+	for (int i = 0; i < 19; i++)
+	{
+		for (int j = 0; j < 19; j++)
+		{
+			copygrid[i][j] = grid[i][j];
+			if (copygrid[i][j] != b && coprygrid[i][j] != w)
+				copygrid[i][j] == e;
+		}
+	}
+
+	copygrid[x][y] == b;
+
+	// 추가 경우의 수
+	// B B 빈칸 B B 빈칸 B B
+	for (int j = 0; j < 12; j++) // 가로
+	{
+		if (copygrid[j][y] == b && copygrid[j + 1][y] == b && copygrid[j + 2][y] == e && copygrid[j + 3][y] == b && copygrid[j + 4][y] == b && copygrid[j + 5][y] == e && copygrid[j + 6][y] == b && copygrid[j + 7][y] == b)
+			return false;
+	}
+
+	for (int i = 0; i < 12; i++) // 세로
+	{
+		if (copygrid[x][i] == b && copygrid[x][i + 1] == b && copygrid[x][i + 2] == e && copygrid[x][i + 3] == b && copygrid[x][i + 4] == b && copygrid[x][i + 5] == e && copygrid[x][i + 6] == b && copygrid[x][i + 7] == b)
+			return false;
+	}
+
+	for (int j = 0; j < 12; j++) // 우측으로 떨어지는 대각선
+	{
+		for (int i = 0; i < 12; i++)
+		{
+			if (copygrid[j][i] == b && copygrid[j + 1][i + 1] == b && copygrid[j + 2][i + 2] == e && copygrid[j + 3][i + 3] == b && copygrid[j + 4][i + 4] == b && copygrid[j + 5][i + 5] == e && copygrid[j + 6][i + 6] == b && copygrid[j + 7][i + 7] == b)
+				return false;
+		}
+	}
+	for (int j = 7; j < 19; j++) // 좌측으로 떨어지는 대각선
+	{
+		for (int i = 0; i < 12; i++)
+		{
+			if (copygrid[j][i + 7] == b && copygrid[j - 1][i + 6] == b && copygrid[j - 2][i + 5] == e && copygrid[j - 3][i + 4] == b && copygrid[j - 4][i + 3] == b && copygrid[j - 5][i + 2] == e && copygrid[j - 6][i + 1] == b && copygrid[j - 7][i] == b)
+				return false;
+		}
+	}
+
+	// B 빈칸 B B B 빈칸 B
+
+	for (int j = 0; j < 13; j++) // 가로
+	{
+		if (copygrid[j][y] == b && copygrid[j + 1][y] == e && copygrid[j + 2][y] == b && copygrid[j + 3][y] == b && copygrid[j + 4][y] == b && copygrid[j + 5][y] == e && copygrid[j + 6][y] == b)
+			return false;
+	}
+
+
+	for (int i = 0; i < 13; i++) // 세로
+	{
+		if (copygrid[x][i] == b && copygrid[x][i + 1] == e && copygrid[x][i + 2] == b && copygrid[x][i + 3] == b && copygrid[x][i + 4] == b && copygrid[x][i + 5] == e && copygrid[x][i + 6] == b)
+			return false;
+	}
+
+	for (int j = 0; j < 13; j++) // 우측으로 떨어지는 대각선
+	{
+		for (int i = 0; i < 13; i++)
+		{
+			if (copygrid[j][i] == b && copygrid[j + 1][i + 1] == e && copygrid[j + 2][i + 2] == b && copygrid[j + 3][i + 3] == b && copygrid[j + 4][i + 4] == b && copygrid[j + 5][i + 5] == e && copygrid[j + 6][i + 6] == b)
+				return false;
+		}
+	}
+	for (int j = 6; j < 19; j++) // 좌측으로 떨어지는 대각선
+	{
+		for (int i = 0; i < 13; i++)
+		{
+			if (copygrid[j][i + 6] == b && copygrid[j - 1][i + 5] == e && copygrid[j - 2][i + 4] == b && copygrid[j - 3][i + 3] == b && copygrid[j - 4][i + 2] == b && copygrid[j - 5][i + 1] == e && copygrid[j - 6][i] == b)
+				return false;
+		}
+	}
+
+	return true;
+}
+
+bool check_overline(char** grid, int x, int y)
+{
+	char copygrid[19][19] == {0};
+	for (int i = 0; i < 19; i++)
+	{
+		for (int j = 0; j < 19; j++)
+			copygrid[i][j] = grid[i][j];
+	}
+
+	for (int j = 0; j < 13; j++) // 가로
+	{
+		if (copygrid[j][y] == b && copygrid[j + 1][y] == b && copygrid[j + 2][y] == b && copygrid[j + 3][y] == b && copygrid[j + 4][y] == b && copygrid[j + 5][y] == b)
+			return false;
+	}
+
+
+	for (int i = 0; i < 13; i++) // 세로
+	{
+		if (copygrid[x][i] == b && copygrid[x][i + 1] == b && copygrid[x][i + 2] == b && copygrid[x][i + 3] == b && copygrid[x][i + 4] == b && copygrid[x][i + 5] == b)
+			return false;
+	}
+
+	for (int j = 0; j < 14; j++) // 우측으로 떨어지는 대각선
+	{
+		for (int i = 0; i < 14; i++)
+		{
+			if (copygrid[j][i] == b && copygrid[j + 1][i + 1] == b && copygrid[j + 2][i + 2] == b && copygrid[j + 3][i + 3] == b && copygrid[j + 4][i + 4] == b && copygrid[j + 5][i + 5] == b)
+				return false;
+		}
+	}
+	for (int j = 5; j < 19; j++) // 좌측으로 떨어지는 대각선
+	{
+		for (int i = 0; i < 14; i++)
+		{
+			if (copygrid[j][i + 5] == b && copygrid[j - 1][i + 4] == b && copygrid[j - 2][i + 3] == b && copygrid[j - 3][i + 2] == b && copygrid[j - 4][i + 1] == b && copygrid[j - 5][i] == b)
+				return false;
+		}
+	}
+
+	return true;
+}
+
+bool check_confirm()
+{
+	if (check_double_three == true && check_double_four == true && check_overline == true)
+		return true;
+	else
+		return false;
 }
